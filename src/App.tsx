a101936e9cc387f7d4d65990b973a01effa7e309
iceticshacker7 <iceandcold18@gmail.com>
2024-10-19 03:45:17 +0530

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ExperienceSection from './pages/ExperienceSection';
-import Header from './component/Header';
-import ProfileCard from './component/ProfileCard';
-import SkillsTree from './pages/SkillsTree';
-import Footer from './component/Footer';
-import CertificatesSection from './pages/CertificatesSection';
-import ProjectsSection from './pages/ProjectsSection';
-import HierarchicalLearningTags from './pages/LearningPage';
-import LoginPage from './services/LoginPage';
-import ProjectDetailPage from './component/ProjectDetailsPage';
-import ProjectManagementPage from './services/ProjectManagementPage';
-import CertificateManagementPage from './services/CertificateManagementPage';
-import DirectoryManagementPage from './services/DirectoryManagementPage';
-import LearningContentPage from './component/LearningContentPage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ExperienceSection from "./pages/ExperienceSection";
+import Header from "./component/Header";
+import ProfileCard from "./component/ProfileCard";
+import SkillsTree from "./pages/SkillsTree";
+import Footer from "./component/Footer";
+import CertificatesSection from "./pages/CertificatesSection";
+import ProjectsSection from "./pages/ProjectsSection";
+import HierarchicalLearningTags from "./pages/LearningPage";
+import LoginPage from "./services/LoginPage";
+import ProjectDetailPage from "./component/ProjectDetailsPage";
+import ProjectManagementPage from "./services/ProjectManagementPage";
+import CertificateManagementPage from "./services/CertificateManagementPage";
+import DirectoryManagementPage from "./services/DirectoryManagementPage";
+import LearningContentPage from "./component/LearningContentPage";
 export default function App() {
   return (
-    
     <Router>
       <div className="App">
         <Header />
@@ -23,16 +22,25 @@ export default function App() {
           <Route path="/" element={<ProfileCard />} />
           <Route path="/experience" element={<ExperienceSection />} />
           <Route path="/projects" element={<ProjectsSection />} />
-          <Route path="/resume" element={<SkillsTree/>} />
+          <Route path="/resume" element={<SkillsTree />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/certificates" element={<CertificatesSection />} /> 
+          <Route path="/certificates" element={<CertificatesSection />} />
           <Route path="/learnings" element={<HierarchicalLearningTags />} />
           <Route path="/projects/:id" element={<ProjectDetailPage />} />
-          <Route path="/project-management" element={<ProjectManagementPage />} />
-          <Route path="/certificate-management" element={<CertificateManagementPage />} />
-        <Route path="/learning/:id" element={<LearningContentPage />} />
-
-          <Route path="/directory-management" element={<DirectoryManagementPage />} />
+          <Route
+            path="/project-management"
+            element={<ProjectManagementPage />}
+          />
+          <Route
+            path="/certificate-management"
+            element={<CertificateManagementPage />}
+          />
+          <Route path="/learning/:id" element={<LearningContentPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/directory-management"
+            element={<DirectoryManagementPage />}
+          />
         </Routes>
         <Footer />
       </div>
